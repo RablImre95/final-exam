5cd6115fc5322c15b0169a5ed3753c7466529e20
Imre <rabl.imre95@gmail.com>
2024-12-16 18:22:02 +0100

    finished javascript tasks

--- carRenting.js
+++ carRenting.js
@@ -1,5 +1,20 @@
 function carRenting(cars, distance) {
-  //write your code here
+  if (!Array.isArray(cars) || cars.length === 0) {
+    return null
+  }
+
+  let result = [];
+
+  for (let i = 0; i < cars.length; i++) {
+    const car = cars[i];
+    const maxDistance = (car.fuelAmount / car.avgConsumption) * 100
+
+    if (maxDistance >= distance) {
+      result.push(car.type)
+    }
+  }
+
+  return result.length > 0 ? result : null;
 }
 
 module.exports = carRenting
--- jadenTalk.js
+++ jadenTalk.js
@@ -1,5 +1,16 @@
 function jadenTalk(text) {
-  //write your code here
+
+  text = text.replace(/[.,!?]/g,"")
+
+  let words = text.split(" ")
+
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > 0) {
+      words[i] = words[i][0].toUpperCase() + words[i].slice(1).toLowerCase()
+    }
+  }
+
+  return words.join(" ")
 }
 
 module.exports = jadenTalk
